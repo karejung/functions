444185664a7a793a9176c67805e65d6b89b4b551
karejung <dev@altroom3d.com>
2025-11-21 19:36:28 +0900

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import Scene from '@/components/Scene'
-import Scene2 from '@/components/test/Scene'
-import Scene3 from '@/components/test2/Scene3'
 
 export default function Home() {
   return <Scene />
